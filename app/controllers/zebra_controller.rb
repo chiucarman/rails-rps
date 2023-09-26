71fb6d17d9b910dfab9105a9dae7b02b824d5e87
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-26 16:05:00 +0000

    /rock route complete

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -4,7 +4,15 @@ class ZebraController < ApplicationController
   end
   
   def giraffe
-    @random_move = ["rock", "paper", "scissors"].sample
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
 
     render({ :template => "game_templates/play_rock" })
   end
